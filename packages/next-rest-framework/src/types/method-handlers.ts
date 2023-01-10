dc5b5c1beb709e3c87b274e6275d76b19490d740
Markus Blomqvist <blomqvist_markus@hotmail.com>
2023-01-10 02:27:50 +0200

    Fix typings for multiple outputs
    This fixes an issue when using multiple output objects
    with different types - now the output types are combined
    with a union type to allow multiple differet outputs.

--- packages/next-rest-framework/src/types/method-handlers.ts
+++ packages/next-rest-framework/src/types/method-handlers.ts
@@ -6,21 +6,20 @@ import { Middleware } from './middleware';
 import { TypedNextApiRequest } from './request';
 import { TypedNextApiResponse } from './response';
 import { BaseSchemaType, SchemaReturnType } from './schemas';
-import { Modify } from './utility-types';
 
 export type BaseStatus = number;
 export type BaseContentType = AnyContentTypeWithAutocompleteForMostCommonOnes;
 
 export interface InputObject<
-  BodySchema extends BaseSchemaType = BaseSchemaType
+  InputSchema extends BaseSchemaType = BaseSchemaType
 > {
   contentType: BaseContentType;
-  schema: BodySchema;
+  schema: InputSchema;
 }
 
 export interface OutputObject<
-  Status = unknown,
-  ContentType = unknown,
+  Status extends BaseStatus = BaseStatus,
+  ContentType extends BaseContentType = BaseContentType,
   ResponseSchema extends BaseSchemaType = BaseSchemaType
 > {
   status: Status;
@@ -28,53 +27,48 @@ export interface OutputObject<
   contentType: ContentType;
 }
 
-export type MethodHandler<
-  BodySchema extends BaseSchemaType = BaseSchemaType,
-  Status extends BaseStatus = BaseStatus,
-  ContentType extends BaseContentType = BaseContentType,
-  ResponseSchema extends BaseSchemaType = BaseSchemaType,
+export interface MethodHandler<
+  InputSchema extends BaseSchemaType = BaseSchemaType,
+  Output extends OutputObject = OutputObject,
   GlobalMiddlewareResponse = unknown,
   RouteMiddlewareResponse = unknown,
   MethodMiddlewareResponse = unknown
-> = Modify<
-  OpenAPIV3_1.OperationObject,
-  {
-    input?: InputObject<BodySchema>;
-    output?: Array<OutputObject<Status, ContentType, ResponseSchema>>;
-    middleware?: Middleware<
-      MethodMiddlewareResponse,
-      {
-        params: GlobalMiddlewareResponse & RouteMiddlewareResponse;
-      },
-      TypedNextApiRequest<SchemaReturnType<BodySchema>>,
-      TypedNextApiResponse<
-        Status,
-        ContentType,
-        SchemaReturnType<ResponseSchema>
-      >
-    >;
-    handler: ApiHandler<
-      SchemaReturnType<BodySchema>,
-      Status,
-      ContentType,
-      SchemaReturnType<ResponseSchema>,
-      GlobalMiddlewareResponse,
-      RouteMiddlewareResponse,
-      MethodMiddlewareResponse
-    >;
-    errorHandler?: ErrorHandler<
-      {
-        params: GlobalMiddlewareResponse &
-          RouteMiddlewareResponse &
-          MethodMiddlewareResponse;
-      },
-      TypedNextApiRequest<SchemaReturnType<BodySchema>>,
-      TypedNextApiResponse<
-        Status,
-        ContentType,
-        SchemaReturnType<ResponseSchema>
-      >
-    >;
-    openApiSpec?: OpenAPIV3_1.OperationObject;
-  }
->;
+> {
+  input?: InputObject<InputSchema>;
+  output?: Output[];
+  middleware?: Middleware<
+    MethodMiddlewareResponse,
+    {
+      params: GlobalMiddlewareResponse & RouteMiddlewareResponse;
+    },
+    TypedNextApiRequest<SchemaReturnType<InputSchema>>,
+    TypedNextApiResponse<
+      Output['status'],
+      Output['contentType'],
+      SchemaReturnType<Output['schema']>
+    >
+  >;
+  handler: ApiHandler<
+    SchemaReturnType<InputSchema>,
+    Output['status'],
+    Output['contentType'],
+    SchemaReturnType<Output['schema']>,
+    GlobalMiddlewareResponse,
+    RouteMiddlewareResponse,
+    MethodMiddlewareResponse
+  >;
+  errorHandler?: ErrorHandler<
+    {
+      params: GlobalMiddlewareResponse &
+        RouteMiddlewareResponse &
+        MethodMiddlewareResponse;
+    },
+    TypedNextApiRequest<SchemaReturnType<InputSchema>>,
+    TypedNextApiResponse<
+      Output['status'],
+      Output['contentType'],
+      SchemaReturnType<Output['schema']>
+    >
+  >;
+  openApiSpec?: OpenAPIV3_1.OperationObject;
+}
