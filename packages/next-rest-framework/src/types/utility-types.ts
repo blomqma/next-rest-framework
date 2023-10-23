b2867af8bf409564d0b101d16a19130872676f54
Markus Blomqvist <blomqvist_markus@hotmail.com>
2023-10-24 02:45:06 +0300

    Add strong types for response content type headers

--- packages/next-rest-framework/src/types/utility-types.ts
+++ packages/next-rest-framework/src/types/utility-types.ts
@@ -1,13 +1,3 @@
 export type Modify<T, R> = Omit<T, keyof R> & R;
 
-// Accept a string with any casing - used for validating headers.
-// Ref: https://stackoverflow.com/a/64932909
-export type AnyCase<T extends string> = string extends T
-  ? string
-  : T extends `${infer F1}${infer F2}${infer R}`
-  ? `${Uppercase<F1> | Lowercase<F1>}${
-      | Uppercase<F2>
-      | Lowercase<F2>}${AnyCase<R>}`
-  : T extends `${infer F}${infer R}`
-  ? `${Uppercase<F> | Lowercase<F>}${AnyCase<R>}`
-  : '';
+export type AnyCase<T extends string> = T | Uppercase<T> | Lowercase<T>;
