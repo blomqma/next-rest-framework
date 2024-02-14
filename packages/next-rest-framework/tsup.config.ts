ecab60c1b19f17070552598ad256ec13042a8fc9
Markus Blomqvist <blomqvist_markus@hotmail.com>
2024-02-14 23:26:19 +0200

    Update ESBuild config
    This some of the recent changes
    causing and error during `next build`.

--- packages/next-rest-framework/tsup.config.ts
+++ packages/next-rest-framework/tsup.config.ts
@@ -1,4 +1,24 @@
+import { type Plugin } from 'esbuild';
 import { defineConfig } from 'tsup';
+import { readFileSync } from 'fs';
+
+// A Next.js dependency (ua-parser-js) uses __dirname, which is not supported in Edge environment.
+const uaParserDirnamePlugin = (): Plugin => {
+  return {
+    name: 'dirname-plugin',
+    setup(build) {
+      build.onLoad({ filter: /\/ua-parser-js\// }, async (args) => {
+        let contents = readFileSync(args.path, 'utf8');
+        contents = contents.replace(/__dirname/g, '');
+
+        return {
+          contents,
+          loader: 'js'
+        };
+      });
+    }
+  };
+};
 
 export default defineConfig({
   entry: [
@@ -8,7 +28,7 @@ export default defineConfig({
     'src/cli/index.ts'
   ],
   bundle: true,
+  esbuildPlugins: [uaParserDirnamePlugin()],
   format: ['cjs', 'esm'],
-  external: ['zod'],
   platform: 'node'
 });
