80fb375a0123f47544d70fd6af904d151e08d3a0
Markus Blomqvist <blomqvist_markus@hotmail.com>
2024-02-26 21:32:01 +0200

    Fix query parameter  OpenAPI generation (#138)
    The OpenAPI spec generation for query
    parameters ended up adding unnecessary
    union types for the generated schema which
    is fixed here.
    
    The query parameter parsing is now done with
    the `qs` library instead of the previous manual
    parsing and some test coverage is added to handle
    both valid and invalid query parameter calls.
    
    Co-authored-by: itsjxck <1731116+itsjxck@users.noreply.github.com>

---
+++ apps/example/src/app/api/v2/route-with-query-params/route.ts
@@ -0,0 +1,37 @@
+import { TypedNextResponse, route, routeOperation } from 'next-rest-framework';
+import { z } from 'zod';
+
+const schema = z.object({
+  foo: z.string().uuid(),
+  bar: z.string().optional(),
+  baz: z.string()
+});
+
+export const dynamic = 'force-dynamic';
+
+// Example app router route handler with query params.
+export const { GET } = route({
+  getQueryParams: routeOperation({
+    method: 'GET'
+  })
+    .input({
+      contentType: 'application/json',
+      query: schema
+    })
+    .outputs([
+      {
+        status: 200,
+        contentType: 'application/json',
+        schema
+      }
+    ])
+    .handler((req) => {
+      const query = req.nextUrl.searchParams;
+
+      return TypedNextResponse.json({
+        foo: query.get('foo') ?? '',
+        bar: query.get('bar') ?? '',
+        baz: query.get('baz') ?? ''
+      });
+    })
+});
