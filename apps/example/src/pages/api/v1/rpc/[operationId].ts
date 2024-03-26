56e472169161d286ac37bc91722c6d36a382d553
Markus Blomqvist <blomqvist_markus@hotmail.com>
2024-03-27 00:59:32 +0200

    Improve RPC route response serialization
    This change serializes Blob data for API routes
    and creates a JSON object from form responses,
    although these are rare cases.

--- apps/example/src/pages/api/v1/rpc/[operationId].ts
+++ apps/example/src/pages/api/v1/rpc/[operationId].ts
@@ -8,6 +8,14 @@ import {
 } from '@/actions';
 import { rpcApiRoute } from 'next-rest-framework';
 
+// Body parser must be disabled when parsing multipart/form-data requests with pages router.
+// A recommended way is to create a separate RPC API route for multipart/form-data requests.
+// export const config = {
+//   api: {
+//     bodyParser: false
+//   }
+// };
+
 const handler = rpcApiRoute({
   getTodos,
   getTodoById,
--- packages/next-rest-framework/src/shared/utils.ts
+++ packages/next-rest-framework/src/shared/utils.ts
@@ -5,3 +5,17 @@ export const isValidMethod = (x: unknown): x is ValidMethod =>
 
 export const capitalizeFirstLetter = (str: string) =>
   str[0]?.toUpperCase() + str.slice(1);
+
+export const parseRpcOperationResponseJson = async (res: unknown) => {
+  if (res instanceof FormData || res instanceof URLSearchParams) {
+    const body: Record<string, FormDataEntryValue> = {};
+
+    for (const [key, value] of res.entries()) {
+      body[key] = value;
+    }
+
+    return body;
+  }
+
+  return res;
+};
