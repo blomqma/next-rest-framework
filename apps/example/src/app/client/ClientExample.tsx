f76134eba1d3f6dcdc64f6d27e0f44fb748c8535
Markus Blomqvist <blomqvist_markus@hotmail.com>
2025-04-13 23:06:44 +0300

    Handle route groups for generated OpenAPI spec (#191)
    With this change, app router routes under route group
    folders are handled so that the route group name is not
    included in the generated OpenAPI spec.

--- apps/example/src/app/client/ClientExample.tsx
+++ apps/example/src/app/client/ClientExample.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { type RpcClient } from '@/app/api/v2/rpc/[operationId]/route';
 import { rpcClient } from 'next-rest-framework/dist/client';
 import { useEffect, useState } from 'react';
 import { type Todo } from '@/utils';
+import { type RpcClient } from '../(main)/api/v2/rpc/[operationId]/route';
 
 const client = rpcClient<RpcClient>({
   url: 'http://localhost:3000/api/v2/rpc'
